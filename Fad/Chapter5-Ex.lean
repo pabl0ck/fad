ada739f00deae3ec8056738685f9c8575fb677e4
Pablo Andrade Carvalho Barros <sirpablo4@gmail.com>
2024-10-25 00:16:33 -0300

    Exercício 5.3

--- Fad/Chapter5-Ex.lean
+++ Fad/Chapter5-Ex.lean
@@ -1,7 +1,19 @@
 import Fad.Chapter5
+import Fad.Chapter4
 
 namespace Chapter5
 
-/- 5.8 : see book -/
+-- 5.3
+
+open Tree1
+
+def qcat (xs : List Nat) (ys : List Nat) : List Nat :=
+  Tree1.Tree.flatcat (mkTree xs) ys
+
+def qsort (xs : List Nat) : List Nat :=
+  qcat xs []
+
+#eval qsort [3, 1, 4, 1, 5, 9, 2, 6, 5]
+
 
 end Chapter5
